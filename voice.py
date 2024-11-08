3c6185b35ea237c588caf3f8ec559cb7a5cd50b6
lifeng <lifeng@zingfront.com>
2024-11-08 16:36:41 +0800

    版本

--- voice.py
+++ voice.py
@@ -1,11 +1,11 @@
 import torch
 from TTS.api import TTS
-
+from pydub import AudioSegment
 # Get device
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # List available 🐸TTS models
-print(TTS().list_models())
+# print(TTS().list_models())
 # print(device)
 # Init TTS
 tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
@@ -15,4 +15,13 @@ tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
 # # Text to speech list of amplitude values as output
 # wav = tts.tts(text="Hello world!", speaker_wav="6448186ce6a310d12f2d1d2c.mp3", language="en", file_path="output.wav")
 # # Text to speech to a file
-tts.tts_to_file(text="Since this model is multi-lingual voice cloning model, we must set the target speaker_wav and language",speed=0.1, speaker_wav="6448186ce6a310d12f2d1d2c.mp3", emotion="sleepy", language="en", file_path="output.wav")
+path = tts.tts_to_file(text="Since this model is multi-lingual voice cloning model, we must set the target speaker_wav and language",speed=0.1, speaker_wav="6448186ce6a310d12f2d1d2c.mp3", emotion="sleepy", language="en", file_path="output.wav")
+
+print(path)
+
+# 读取音频文件
+audio = AudioSegment.from_file(path)
+# 调整音频速度
+new_audio = audio.speedup(playback_speed=0.5)
+# 保存新的音频文件
+new_audio.export("outputslow.wav", format="wav")
